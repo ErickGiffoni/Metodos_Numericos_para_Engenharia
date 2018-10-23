bae71a87a21778ba28502217cae8cf632acef666
ErickGiffoni <giffoni.erick@gmail.com>
2018-10-23 19:39:25 -0300

    Few checks

--- trabalho_3_grid/grid.c
+++ trabalho_3_grid/grid.c
@@ -3,10 +3,14 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 #define ordem_da_matriz 9 // a ordem deve ser impar, para termos um elemento central
 
 int main(){
+
+  int elemento;
+  int random_number;
 
   int **matriz;
 
@@ -32,7 +36,7 @@ int main(){
 
   /* Condicao principal -> Preencher 10% de ordem_da_matriz^2 vezes */
 
-float condicao_principal = (10f/100f) * pow(ordem_da_matriz, 2);
+float condicao_principal = (10.0/100.0) * pow(ordem_da_matriz, 2);
 float verifica_condicao_principal = 0;
 
 
@@ -44,15 +48,15 @@ float verifica_condicao_principal = 0;
       do{ // escolher um elemnto na matriz de forma aleatoria
         srand(time(NULL));
 
-        int elemento = rand() % ordem_da_matriz; // random number from 0 up to 8 if order = 9
+        elemento = rand() % ordem_da_matriz; // random number from 0 up to 8 if order = 9
 
-      }while(matriz[elemento][elemento] != 0;) // repete ate que ache um elemento na matriz que nao seja o central, que e igual a 1
+      }while(matriz[elemento][elemento] != 0); // repete ate que ache um elemento na matriz que nao seja o central, que e igual a 1
 
       // escolher um numero de 1 a 4 aleatoriamente -> condicao 3
-      condicao3:
+    condicao3:
       srand(time(NULL));
 
-      int random_number = rand() % 4 + 1; //random number from 1 up to 4
+      random_number = rand() % 4 + 1; //random number from 1 up to 4
 
       switch(random_number){
         case 1: // go right
