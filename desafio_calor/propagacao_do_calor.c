a31b4cb06dd1b42a59ac7659fb545c26729f863d
ErickGiffoni <giffoni.erick@gmail.com>
2018-11-12 14:15:26 -0200

    Falta fazer real time em grafico

--- desafio_calor/propagacao_do_calor.c
+++ desafio_calor/propagacao_do_calor.c
@@ -3,34 +3,34 @@
 #include <math.h>
 #include <time.h>
 
-#define tamanho 10
+#define tamanho 20
 
 int main (){
   int i,j;
 
-  int **matriz = (int **)calloc(tamanho,sizeof(int *));
+  double **matriz = (double **)calloc(tamanho,sizeof(double *));
   for(i=0; i<tamanho; i++){
-    matriz[i] = (int *)calloc(tamanho,sizeof(int));
+    matriz[i] = (double *)calloc(tamanho,sizeof(double));
   }//alocacao da matriz
 
 
   for(i=1; i<tamanho-1; i++){ //percorrer a coluna 0
-    matriz[i][0] = 20;
+    matriz[i][0] = 20.0;
   }
   for(i=1; i<tamanho-1; i++){ //percorrer a coluna 99
-    matriz[i][tamanho-1] = 10;
+    matriz[i][tamanho-1] = 10.0;
   }
   for(i=1; i<tamanho-1; i++){ //percorrer a linha 0
-    matriz[0][i] = 40;
+    matriz[0][i] = 40.0;
   }
   for(i=1; i<tamanho-1; i++){ //percorrer a linha 99
-    matriz[tamanho-1][i] = 10;
+    matriz[tamanho-1][i] = 10.0;
   }
 
 printf("Inicial:\n\n");
   for(i=0;i<tamanho;i++){
     for(j=0;j<tamanho;j++){
-      printf("\t%d ", matriz[i][j]);
+      printf("\t%.3lf ", matriz[i][j]);
     }
     printf("\n");
   }
@@ -45,12 +45,21 @@ printf("Inicial:\n\n");
     i = rand() % tamanho1+1;
     j = rand() % tamanho1+1;
     matriz[i][j] = (matriz[i-1][j]+matriz[i][j-1]+matriz[i+1][j]+matriz[i][j+1])/4;
+    for(i=0;i<tamanho;i++){
+      for(j=0;j<tamanho;j++){
+        printf("\t%.3lf ", matriz[i][j]);
+      }
+      printf("\n");
+    }
+    system("clear");
     counter++;
-  }while(counter<=pow(tamanho,2));
+  }while(1);
+
+  // system("cls");
 
   for(i=0;i<tamanho;i++){
     for(j=0;j<tamanho;j++){
-      printf("\t%d ", matriz[i][j]);
+      printf("\t%.3lf ", matriz[i][j]);
     }
     printf("\n");
   }
